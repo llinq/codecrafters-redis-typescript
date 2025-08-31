046d5f3e1efa5fd25500aba0fba7314e58526b8d
Joao Costa <joaopedrocosta.contato@gmail.com>
2025-08-31 04:31:33 -0300

    Remove memory state from main

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,5 @@
 import * as net from "net";
 import { RedisProtocol } from "./redis-protocol";
-import { MemoryStore } from "./memory";
-
-const serverStore = new MemoryStore();
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data) => {
