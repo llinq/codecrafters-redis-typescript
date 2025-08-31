6b1fe6c8f2088393252ce3f6ed2acca6e179dfb7
Joao Costa <joaopedrocosta.contato@gmail.com>
2025-08-30 21:35:13 -0300

    Add PONG

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,7 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.write(`+ PONG\r\n`);
 });
 
 server.listen(6379, "127.0.0.1");
